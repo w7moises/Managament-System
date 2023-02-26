b27ed970955d728f18879b672c2bf7842e040f11
Moises Molina <w7moises@gmail.com>
2023-02-26 12:40:43 -0500

    template added for spring boot 3 project

---
+++ src/test/java/com/management/app/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
