9fc223276feae5d27d7c1a15233794492674ad09
Moises Molina <w7moises@gmail.com>
2023-02-26 13:12:20 -0500

    user service added

---
+++ src/main/java/com/management/app/service/UserService.java
@@ -0,0 +1,17 @@
+package com.management.app.service;
+
+import com.management.app.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+}
