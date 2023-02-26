3ae8d2f27ae38aca195ddcc5d4f0ead7222b56f9
Moises Molina <w7moises@gmail.com>
2023-02-26 13:05:38 -0500

    user entity added

---
+++ src/main/java/com/management/app/entity/User.java
@@ -0,0 +1,25 @@
+package com.management.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(nullable = false,unique = true)
+    private String email;
+}
